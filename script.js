785745aa68ae560d69a1ab30e627b197e001431d
Mikolaj <mikolajjankowski98@gmail.com>
2025-04-10 21:13:26 +0200

    10.04

--- script.js
+++ script.js
@@ -28,3 +28,43 @@ clickCounter.addEventListener("click", function () {
   clickCount++;
   clickCounter.textContent = `Liczba kliknięć: ${clickCount}`;
 });
+
+const theme = document.getElementById("theme");
+document.body.classList.add("light");
+theme.addEventListener("click", () => {
+  const isLight = document.body.classList.contains("light");
+  if (isLight) {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    console.log("Dark");
+  } else {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    console.log("Light");
+  }
+  localStorage.setItem("theme", document.body.classList[0]);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const themeSave = localStorage.getItem("theme");
+  document.body.classList.add(themeSave);
+  document.body.classList.remove(themeSave === "light" ? "dark" : "light");
+  theme.textContent = themeSave === "light" ? "Tryb ciemny" : "Tryb jasny";
+});
+
+const list2 = [-3, 5, 6]; //sprawdź czy chociaż jedna wartość z tej listy jest ujemna
+console.log(list2.some((el) => {
+  return el < 0;
+})
+);
+
+const namesList = ["Ania", "Hania", "Frania"];
+const reducesString = namesList.reduce((acc,el,index)=> {
+  console.log(index);
+  if(index < namesList.lenght -1){
+    return acc +el+ ", ";
+  }else{
+    return acc + el;
+  }
+}, "");
+console.log(reducesString);
